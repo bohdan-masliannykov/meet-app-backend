c3eb4d117e1778466e0ca6d3280925910566a33c
Bohdan <bohdan.masliannykov@gmail.com>
2025-06-25 17:04:34 +0200

    feat: init

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/users", () => {
+    return [
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Jane Smith" },
+        { id: 3, name: "Alice Johnson" }
+    ]
+});
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
